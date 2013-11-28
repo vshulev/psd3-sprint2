c7eb9e661de8168aeca542233f3dfb42f14ae9c2
Kritin Singh <2107613s@bo720-6-03.(none)>
2013-11-28 19:09:28 +0000

    Create session adding SessionController.java and its template

---
+++ src/main/java/sprint2/SessionController.java
@@ -0,0 +1,31 @@
+package sprint2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SessionController {
+
+
+
+	@RequestMapping("session/create")
+	public String create() {
+		return "session/create";
+	}
+
+	//@RequestMapping("/session/create")
+	public String session(
+@RequestParam(value="time", required=true) String time, 
+@RequestParam(value="duration_dropdown", required=true) String duration , 
+@RequestParam(value="frequence", required=true) String frequence, 
+@RequestParam(value="lecture", required=true) String lecture , 
+@RequestParam(value="max_attendance", required=true) String max_attendance ,
+@RequestParam(value="compolsory", required=true) String compolsory 
+
+) {
+		return "login";
+	}
+
+}
